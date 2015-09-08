a6135567bb78c5203467b5544489b0f4234f1325
Ireneusz Luczak <luczak.irek@gmail.com>
2015-09-08 14:12:16 -0500

    got a model to work

--- public/js/controllers/ResultsController.js
+++ public/js/controllers/ResultsController.js
@@ -1,3 +1,13 @@
 hashTrack.controller('ResultsController', ['$scope', function($scope) {
-
+  $scope.apps = [{
+  hashtag: '#pizza',
+  users: 14,
+  tweets: 18
+  },
+  {
+  hashtag: '#burgers',
+  users: 9,
+  tweets: 14
+  }
+]
 }]);
