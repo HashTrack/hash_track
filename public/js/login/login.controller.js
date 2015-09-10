fe9d8ec82ebb1e732efda025a1db420f7954ff96
Ireneusz Luczak <luczak.irek@gmail.com>
2015-09-10 10:16:54 -0500

    fumbling with login form

--- public/js/login/login.controller.js
+++ public/js/login/login.controller.js
@@ -1,7 +1,9 @@
 hashTrack.controller('LoginController', ['$scope',  function($scope) {
 
-	$scope.submitAuth = function(email, password, authType) {
-		console.log(email);
-	}
+	$scope.submitAuth = function(email, password) {
+    console.log('hello');
+    $scope.email = email;
+		console.log('we got here' + email + password + authType);
+	};
 
 }]);
--- public/js/search/search.controller.js
+++ public/js/search/search.controller.js
@@ -1,9 +1,8 @@
 hashTrack.controller('SearchController', ['$scope', function($scope, $location) {
   $scope.searchHashtag = function(hashtag) {
     $scope.hashtag = hashtag;
-    $location.url('#/results');
+    // $location.url('#/results');
     console.log('This thing needs to route corectly and its searching for hashtag: #' + hashtag);
-
   };
 
 }]);
