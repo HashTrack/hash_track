6c87170b35761f629577c9cc0b797de28753203c
Ireneusz Luczak <luczak.irek@gmail.com>
2015-09-10 13:40:38 -0500

    minor log changed

--- public/js/search/search.controller.js
+++ public/js/search/search.controller.js
@@ -1,9 +1,9 @@
-hashTrack.controller('SearchController', ['$scope', '$http', '$location', function($scope, $http, $location) {
+hashTrack.controller('SearchController', ['$scope', '$location', function($scope, $location) {
+
   $scope.searchHashtag = function(hashtag) {
     $scope.hashtag = hashtag;
     $scope.newURL = ('/results/' + $scope.hashtag)
-    console.log($scope.newURL);
-    console.log('This thing needs to route corectly and its searching for hashtag: #' + hashtag);
+    console.log('Sending you a tasty view at route : #/results/' + hashtag);
     $location.path($scope.newURL);
   };
 
