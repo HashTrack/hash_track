8a6f0cee2932a712e096a712a1723e4a09f4b930
Ireneusz Luczak <luczak.irek@gmail.com>
2015-09-10 10:00:54 -0500

    commit

--- public/js/search/search.controller.js
+++ public/js/search/search.controller.js
@@ -1,7 +1,9 @@
-hashTrack.controller('SearchController', ['$scope', function($scope) {
+hashTrack.controller('SearchController', ['$scope', function($scope, $location) {
   $scope.searchHashtag = function(hashtag) {
     $scope.hashtag = hashtag;
+    $location.url('#/results');
     console.log('This thing needs to route corectly and its searching for hashtag: #' + hashtag);
+
   };
 
 }]);
