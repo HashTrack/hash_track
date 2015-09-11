e2e47cd92925ff8a9cf4ef2acd3e638f1fab5f9f
Matthew Herron <matthew.herron77@gmail.com>
2015-09-11 14:53:52 -0500

    Fixed nav width

--- public/js/common/header.controller.js
+++ public/js/common/header.controller.js
@@ -1,6 +1,6 @@
 hashTrack.controller('HeaderController', ['$scope', '$rootScope', 'authentication', '$location', function($scope, $rootScope, authentication, $location) {
 	$scope.loggedIn = function() { return authentication.isLoggedIn(); };
-	
+	if ($scope.loggedIn) $rootScope.currentUser = authentication.currentUser().email_address;
 	console.log('$scope.loggedIn = ' + $scope.loggedIn());
 
 	$scope.signout = function() {
