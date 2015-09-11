73234bdc6977bf8bcb53d883c21a4f8e2ee72332
Matthew Herron <matthew.herron77@gmail.com>
2015-09-11 12:05:12 -0500

    spinners

--- public/js/results/results.controller.js
+++ public/js/results/results.controller.js
@@ -6,8 +6,13 @@ hashTrack.controller('ResultsController', ['$scope', 'searchNoGeo', '$routeParam
   }
   ];
 
+  $scope.user_spinner = false;
+  $scope.tweet_spinner = false;
+
   $scope.getDataNoGeo = function (callback_1, callback_2) {
     $scope.hashtagData = {};
+    $scope.user_spinner = true;
+    $scope.tweet_spinner = true;
     searchNoGeo.getTweets($routeParams.hashtag)
       .success(function(data) {
         callback_1(data);
@@ -26,6 +31,7 @@ hashTrack.controller('ResultsController', ['$scope', 'searchNoGeo', '$routeParam
         uniqueData.push(screenName);
       };
     }
+    $scope.user_spinner = false;
     $scope.apps[0].users = uniqueData.length;
     console.log(uniqueData.length);
 
