53f149a01398d055eb35a4cdc496d75f1c21b7f9
Matthew Herron <matthew.herron77@gmail.com>
2015-09-11 12:51:38 -0500

    figuring out the query string thing

--- public/js/search/search.controller.js
+++ public/js/search/search.controller.js
@@ -1,10 +1,17 @@
 hashTrack.controller('SearchController', ['$scope', '$location', function($scope, $location) {
 
-  $scope.searchHashtag = function(hashtag) {
-    $scope.hashtag = hashtag;
-    $scope.newURL = ('/results/' + $scope.hashtag)
-    console.log('Sending you a tasty view at route : #/results/' + hashtag);
-    $location.path($scope.newURL);
+  $scope.searchHashtag = function(hashtags) {
+  	hashtags = hashtags.split(',');
+  	console.log(typeof hashtags);
+    var hashTagQuery = '';
+    hashtags.forEach(function(hashtag) {
+    	hashTagQuery += 'h=' + hashtag.replace(/\s/g, '') + '&';
+    });
+    hashTagQuery = hashTagQuery.substring(0, hashTagQuery.length - 1);
+    $scope.newQuery = (hashTagQuery);
+    console.log('Sending you a tasty new URL : ' + $scope.newQuery);
+    $location.search($scope.newQuery);
+    $location.path('/results');
   };
 
 }]);
