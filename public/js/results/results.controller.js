f6c0350209ee33add351e30963528289e9ca159c
Ireneusz Luczak <luczak.irek@gmail.com>
2015-09-11 10:29:21 -0500

    got callback functions working

--- public/js/results/results.controller.js
+++ public/js/results/results.controller.js
@@ -6,30 +6,35 @@ hashTrack.controller('ResultsController', ['$scope', 'searchNoGeo', '$routeParam
     }
   ];
 
-  $scope.getDataNoGeo = function () {
+  $scope.getDataNoGeo = function (callback_1, callback_2) {
     $scope.hashtagData = {};
     searchNoGeo.getTweets($routeParams.hashtag)
       .success(function(data) {
-        $scope.hashtagData = data
+        callback_1(data);
+        callback_2(data);
       })
       .error(function (e) {
         console.log('You goofed somewhere...');
+
   })};
 
   $scope.grabUniqueUsers = function (data) {
     var json = data, i, l;
     console.log('data in unique function');
     console.log(json);
-  };
-
-  $scope.grabUniqieTweets = function (data) {
 
   };
 
+  $scope.grabUniqueTweets = function (data) {
+    console.log('-------------');
+    console.log('tweets function');
+    console.log('-------------');
+  };
 
-  $scope.getDataNoGeo();
-  $scope.grabUniqueUsers($scope.hashtagData);
-  // $scope.grabUniqieTweets($scope.hashtagData);
+
+  $scope.getDataNoGeo($scope.grabUniqueUsers, $scope.grabUniqueTweets);
+
+
 
 
 }]);
